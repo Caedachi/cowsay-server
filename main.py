8a792b187ec5fb0a0a36819aeb1efede349b09df
Caedachi <cae+github@caedachi.art>
2021-08-14 22:38:55 +0000

    add optional port arg and add requirements

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from server import CowsayServer
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='moo')
     parser.add_argument('--file', type=str, required=True, help='phrases file')
+    parser.add_argument('--port', type=int, required=False, default=8080, help='port to bind to')
 
     args = parser.parse_args()
+
+    cherrypy.config.update({'server.socket_port': args.port})
 
     cherrypy.quickstart(CowsayServer(args.file))
